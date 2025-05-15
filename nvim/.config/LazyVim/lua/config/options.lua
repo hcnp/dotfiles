67f3ee20e517205a0ee6ed07c1aa981ed92c6eae
Henning C. Nielsen <hcnp@trifork.com>
2025-05-15 09:07:01 +0200

    refactor(LazyVim): Mason path config is no longer needed

--- nvim/.config/LazyVim/lua/config/options.lua
+++ nvim/.config/LazyVim/lua/config/options.lua
@@ -2,6 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.env.PATH = vim.env.PATH .. ":~/.local/share/LazyVim/mason/bin/"
 vim.g.autoformat = false
 vim.opt.colorcolumn = "80,120"
